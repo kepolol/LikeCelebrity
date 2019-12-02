d49aeb829f27ff4ad8c1608b73bcb11a816a534f
BLOOMFLARK <50524041+BLOOMFLARK@users.noreply.github.com>
2019-12-02 16:49:05 +0300

    with try-catch

--- ML/image_preprocess.py
+++ ML/image_preprocess.py
@@ -8,11 +8,9 @@ from numpy import expand_dims
 
 
 def face2embedding(model, data):
-    embeddings = list()
-    #for face_pixels in data:
     emb = get_embedding(model, data)
-    #    embeddings.append(emb)
     return asarray(emb)
+
 
 def get_embedding(model, face_pixels):
     face_pixels = face_pixels.astype('float32')
@@ -28,27 +26,27 @@ def get_embedding(model, face_pixels):
     yhat = model.predict(samples)
     return yhat[0]
 
+
 def extract_face(filename, required_size=(160, 160)):
     image = Image.open(filename)
     image = image.convert('RGB')
     pixels = asarray(image)
     
     detector = MTCNN()
-    
     # detect faces in the image
     results = detector.detect_faces(pixels)
 
     # extract the bounding box from the first face
-    #try:
-    x1, y1, width, height = results[0]['box']
-    x1, y1 = abs(x1), abs(y1)
-    x2, y2 = x1 + width, y1 + height
-    #except Exception:
-    #    print("There is no face or there are too many, load another photo!")
-    #    return
-
-    # extract the face
-    face = pixels[y1:y2, x1:x2]
-    image = Image.fromarray(face)
-    image = image.resize(required_size)
-    return asarray(image)
+    try:
+        x1, y1, width, height = results[0]['box']
+    except IndexError:
+        print("There is no face on the photo!")
+        return
+    else:
+        x1, y1 = abs(x1), abs(y1)
+        x2, y2 = x1 + width, y1 + height
+        # extract the face
+        face = pixels[y1:y2, x1:x2]
+        image = Image.fromarray(face)
+        image = image.resize(required_size)
+        return asarray(image)
