0741e80333bbeab45133b0433db22c4b64c278ca
Anton35925 <Anton25925@gmail.com>
2019-12-02 15:43:41 +0300

    for_base

---
+++ cel_base/script_create.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, Binary, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+
+Base = declarative_base()
+
+
+class ImageTable(Base):
+    __tablename__ = "Images"
+    
+    id_ = Column(Integer, primary_key=True)
+    key = Column(Integer)
+    name = Column(String)
+    image = Column(Binary)
+
+
+engine = create_engine('sqlite:///celebrities.db')
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(bind=engine)
