612259debb5f4512bf05ff572ed0bea6b620d606
Anton35925 <Anton35925@gmail.com>
2019-12-03 20:20:02 +0200

    update_test_web

--- tests/test_WEB.py
+++ tests/test_WEB.py
@@ -3,7 +3,19 @@ sys.path.append("vkbot/")
 from vk_bot import bot_response
 
 
-def test_response():
+def test_many_attach():
     message = ""
     resp = bot_response(message, None)
     assert resp == u"Я могу обработать только одну фотографию."
+
+
+def test_other_message1():
+    message = "хай"
+    resp = bot_response(message, None)
+    assert resp == u"Не понимаю тебя.\nПопробуй отправить мне свою фотографию!"
+
+
+def test_other_message2():
+    message = "asdgsa"
+    resp = bot_response(message, None)
+    assert resp == u"Не понимаю тебя.\nПопробуй отправить мне свою фотографию!"
