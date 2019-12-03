d2511af7b5a052be55a6017abe65f155768503af
kepolol <kepolol@gmail.com>
2019-12-03 16:38:03 +0300

    fix some things

--- tests/ML_test.py
+++ tests/ML_test.py
@@ -1,2 +1,10 @@
-import pytest
-from ML.blackbox import Blackbox
+import sys
+sys.path.append("../ML")
+from blackbox import Blackbox
+
+model = Blackbox(1)
+
+
+def face_detection():
+    print(sys.path)
+    assert model.send_picture('_test.jpg')[0] == [7]
